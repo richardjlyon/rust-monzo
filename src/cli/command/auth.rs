2211b4007338adf4ce338a9144c8844c0104ffba
Richard Lyon <richlyon@mac.com>
2024-06-09 11:02:11 +0100

    move server into auth command

--- src/cli/command/auth.rs
+++ src/cli/command/auth.rs
@@ -1,17 +1,47 @@
 use std::collections::HashMap;
 
+use axum::{routing::get, Router};
+
+use tokio::time::{sleep, Duration};
+use tokio_util::sync::CancellationToken;
 use url::Url;
 use uuid::Uuid;
 
 use crate::configuration::get_configuration;
+use crate::routes::oauth_callback;
 
 pub async fn auth() {
     let config = get_configuration().unwrap();
+
+    // Create server
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+        .await
+        .unwrap();
+    let app = Router::new().route("/oauth/callback", get(oauth_callback));
+
+    let token = CancellationToken::new();
+    let cloned_token = token.clone();
+
+    let _server_handle = tokio::task::spawn(async move {
+        tokio::select! {
+            // Step 3: Using cloned token to listen to cancellation requests
+            _ = cloned_token.cancelled() => {
+                // The token was cancelled, task can shut down
+            }
+            _ = tokio::time::sleep(std::time::Duration::from_secs(10)) => {
+                // Long work has completed
+            }
+        }
+        let _ = axum::serve(listener, app).await;
+    });
+
     open_login_page(
         &config.oath_credentials.client_id,
         &config.oath_credentials.redirect_uri,
     )
     .await;
+
+    sleep(Duration::from_secs(60)).await;
 }
 
 async fn open_login_page(client_id: &str, redirect_uri: &str) {
--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,13 @@
 use anyhow::Error;
-use axum::{routing::get, Router};
 use clap::Parser;
 use cli::{command, Cli, Commands};
-use monzo::routes::oauth_callback;
-use tokio_util::sync::CancellationToken;
 
 mod cli;
 mod configuration;
+mod routes;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    // Create server
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
-        .await
-        .unwrap();
-    let app = Router::new().route("/oauth/callback", get(oauth_callback));
-
-    let token = CancellationToken::new();
-    let cloned_token = token.clone();
-
-    let _server_handle = tokio::task::spawn(async move {
-        tokio::select! {
-            // Step 3: Using cloned token to listen to cancellation requests
-            _ = cloned_token.cancelled() => {
-                // The token was cancelled, task can shut down
-            }
-            _ = tokio::time::sleep(std::time::Duration::from_secs(10)) => {
-                // Long work has completed
-            }
-        }
-        let _ = axum::serve(listener, app).await;
-    });
-
     let cli = Cli::parse();
 
     match &cli.command {
