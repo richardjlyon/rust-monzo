6d1c74a469055ffb05c00fb30643ad5f9e59e4c7
Richard Lyon <richlyon@mac.com>
2024-06-09 16:46:03 +0100

    combine and sort transactions

--- src/cli/command/update.rs
+++ src/cli/command/update.rs
@@ -9,14 +9,60 @@ use crate::client::{
 pub async fn update() -> Result<(), Error> {
     let monzo = MonzoClient::new()?;
 
-    println!("{:>77}", "TRANSACTIONS");
-    println!("-----------------------------------------------------------------------------");
+    println!("{:>85}", "TRANSACTIONS");
+    println!(
+        "-------------------------------------------------------------------------------------"
+    );
 
-    let (since, before) = make_date_range(2024, 5); // TODO: use command parameters
-
+    let transactions = get_sorted_transactions().await?;
     let account_description = monzo.account_description_from_id().await?;
 
-    let _txs: Vec<Transaction> = Vec::new();
+    for tx in transactions {
+        let account_name = account_description.get(&tx.account_id).unwrap();
+
+        let iso_code = iso::find(&tx.currency).unwrap();
+        let local_iso_code = iso::find(&tx.local_currency).unwrap();
+
+        let date_fmt = tx.created.format("%Y-%m-%d").to_string();
+        let amount_fmt = Money::from_minor(tx.amount, iso_code).to_string();
+
+        let credit_fmt = match tx.amount >= 0 {
+            true => amount_fmt.clone(),
+            false => "".to_string(),
+        };
+
+        let debit_fmt = match tx.amount < 0 {
+            true => amount_fmt,
+            false => "".to_string(),
+        };
+
+        let local_amount_fmt = match iso_code == local_iso_code {
+            true => "".to_string(),
+            false => format!(
+                "({})",
+                Money::from_minor(tx.local_amount, local_iso_code).to_string()
+            ),
+        };
+
+        let merchant_fmt = match tx.merchant {
+            Some(merchant) => merchant.name,
+            None => "".to_string(),
+        };
+
+        println!(
+            "{:<11} {:<8} {:>12} {:>12} {:>12} {:>25}",
+            date_fmt, account_name, credit_fmt, debit_fmt, local_amount_fmt, merchant_fmt
+        );
+    }
+
+    Ok(())
+}
+
+/// Get all transactions sorted by date
+async fn get_sorted_transactions() -> Result<Vec<Transaction>, Error> {
+    let monzo = MonzoClient::new()?;
+    let (since, before) = make_date_range(2024, 5);
+    let mut txs: Vec<Transaction> = Vec::new();
 
     for account in monzo.accounts().await? {
         let transactions = monzo
@@ -29,43 +75,12 @@ pub async fn update() -> Result<(), Error> {
                 continue;
             }
 
-            let account_name = account_description.get(&tx.account_id).unwrap();
-
-            let iso_code = iso::find(&tx.currency).unwrap();
-            let local_iso_code = iso::find(&tx.local_currency).unwrap();
-
-            let date_fmt = tx.created.format("%Y-%m-%d").to_string();
-            let amount_fmt = Money::from_minor(tx.amount, iso_code).to_string();
-
-            let credit_fmt = match tx.amount >= 0 {
-                true => amount_fmt.clone(),
-                false => "".to_string(),
-            };
-
-            let debit_fmt = match tx.amount < 0 {
-                true => amount_fmt,
-                false => "".to_string(),
-            };
-
-            let local_amount_fmt = match iso_code == local_iso_code {
-                true => "".to_string(),
-                false => format!(
-                    "({})",
-                    Money::from_minor(tx.local_amount, local_iso_code).to_string()
-                ),
-            };
-
-            let merchant_fmt = match tx.merchant {
-                Some(merchant) => merchant.name,
-                None => "".to_string(),
-            };
-
-            println!(
-                "{:<11} {:<8} {:>12} {:>12} {:>12} {:>25}",
-                date_fmt, account_name, credit_fmt, debit_fmt, local_amount_fmt, merchant_fmt
-            );
+            txs.push(tx);
         }
     }
 
-    Ok(())
+    // sort by date
+    txs.sort_by(|a, b| a.created.cmp(&b.created));
+
+    Ok(txs)
 }
